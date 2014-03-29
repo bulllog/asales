0740ca10e49e3e1fc67e6e769ce796c91bbfcc87
Neha Bansal <nehabansal.ducs@gmail.com>
2014-03-29 12:32:37 +0530

    made base.html

--- app/views/home.py
+++ app/views/home.py
@@ -7,5 +7,14 @@ import pybase
 class Home(pybase.PyBase):
     def get(self):
         """ Overrided function of base class"""
-        self.renderTemplate('home.html');
+        categories = ['Men', 'Women', 'Kids', 'Electronics']
+        sub_categories = [
+            'Apparels', 'Footwear', 'Beauty', 'Accessories',
+            'Mobiles And Tablets', 'Computers And Laptops, Home Appliances'
+        ]
+        template_value = {
+            'categories': categories, 'sub_categories': sub_categories,
+            'discounted_items': {}
+        }
+        self.renderTemplate('home.html', template_value);
 
