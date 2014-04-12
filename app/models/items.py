6091dab87bb36ae1c777f8ebbc44fc127aa8fbcb
Neha Bansal <nehabansal.ducs@gmail.com>
2014-04-12 23:40:52 +0530

    adding js files

--- app/models/items.py
+++ app/models/items.py
@@ -8,12 +8,17 @@ import subcategory
 
 
 class Item(ndb.Model):
-    """Models an individual Item entry with category id from category datastore, subcategory id from category datastore, item id, name, price, brand name, has_discount and discount """
-    category = msgprop.EnumProperty(category.Category, required=True)
-    subcategory = msgprop.EnumProperty(subcategory.SubCategory, required=True)
-    item = ndb.StringProperty(required=True)
-    price = ndb.IntegerProperty()
-    brand_name = ndb.StringProperty()
-    has_discount = ndb.BooleanProperty()
-    discount = ndb.FloatProperty()
+  """Models an individual Item entry.
+  
+  The fields are category id from category datastore, subcategory id from
+  category datastore, item id, name, price, brand name, has_discount
+  and discount.
+  """
+  category = msgprop.EnumProperty(category.Category, required=True)
+  subcategory = msgprop.EnumProperty(subcategory.SubCategory, required=True)
+  item = ndb.StringProperty(required=True)
+  price = ndb.IntegerProperty()
+  brand_name = ndb.StringProperty()
+  has_discount = ndb.BooleanProperty()
+  discount = ndb.FloatProperty()
 
--- app/models/subcategory.py
+++ app/models/subcategory.py
@@ -1,15 +1,16 @@
-#This file contain all possible values for subcategories.
-
+# This file contain all possible values for subcategories.
 
 from protorpc import messages
 
+
 class SubCategory(messages.Enum):
-    """Enum for subcategory"""
-    CLOTHING = 1
-    FOOTWEAR = 2
-    ACCESSORIES = 3
-    WATCHES = 4
-    COMPUTERS_LAPTOP = 5
-    MOBILE_TABLET = 6
-    HOME_APPLIANCES = 7
-    CAMERA = 8
+  """Enum for subcategory."""
+  CLOTHING = 1
+  FOOTWEAR = 2
+  ACCESSORIES = 3
+  WATCHES = 4
+  COMPUTERS_LAPTOP = 5
+  MOBILE_TABLET = 6
+  HOME_APPLIANCES = 7
+  CAMERA = 8
+
