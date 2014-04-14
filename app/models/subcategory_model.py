6e1377cba6a4a9ebda798e897666bd7b2a1f3cf0
gittoo <kumar.p6888@gmail.com>
2014-04-14 23:53:35 +0530

    added api to get items on the basis of discounted or non discounted, category and subcategory.

---
+++ app/models/subcategory_model.py
@@ -0,0 +1,15 @@
+#This file contain all possible values for subcategories.
+
+
+from protorpc import messages
+
+class SubCategory(messages.Enum):
+    """Enum for subcategory"""
+    APPARELS = 1
+    FOOTWEAR = 2
+    BEAUTY = 3
+    ACCESSORIES = 4
+    MOBILE_TABLET = 5
+    COMPUTER_LAPTOP = 6
+    HOME_APPLIANCES = 7
+    CAMERA = 8
