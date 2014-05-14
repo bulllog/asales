f88fc0f20ea34213899744043a73da151c9416bf
Neha Bansal <nehabansal.ducs@gmail.com>
2014-05-14 19:22:32 +0530

    some changes

---
+++ app/static/js/soy_templates.js
@@ -0,0 +1,15 @@
+// This file was automatically generated from soy_templates.soy.
+// Please don't edit this file by hand.
+
+if (typeof asales == 'undefined') { var asales = {}; }
+if (typeof asales.templates == 'undefined') { asales.templates = {}; }
+
+
+asales.templates.item = function(opt_data, opt_ignored) {
+  return '';
+};
+
+
+asales.templates.categoryDiscountedItem = function(opt_data, opt_ignored) {
+  return '';
+};
